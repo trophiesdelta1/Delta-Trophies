b9483b1b0e189083c7c09705416d405fa01796d1
Sahib <sahib.ptu@gamil.com>
2026-09-24 11:42:23 +0530

    Enable Vercel Web Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import ScrollToTop from './components/ScrollToTop';
 import AppRoutes from './routes/AppRoutes';
 
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <ScrollToTop />
       <AppRoutes />
+      <Analytics />
     </BrowserRouter>
   );
 }
